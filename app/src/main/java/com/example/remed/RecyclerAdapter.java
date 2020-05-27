f1b6a9c120b02e5c8314569384c48682fb4d2404
Fatmanur Özdemir <ozdemir.fatmanur18@gmail.com>
2020-05-27 22:56:17 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/remed/RecyclerAdapter.java
@@ -0,0 +1,72 @@
+package com.example.remed;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPE = 1;
+    private final Context context;
+    private final List<Object> listRecyclerItem;
+
+    public RecyclerAdapter(Context context, List<Object> listRecyclerItem) {
+        this.context = context;
+        this.listRecyclerItem = listRecyclerItem;
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name;
+
+        public ItemViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.name);
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        switch (i) {
+            case TYPE:
+
+            default:
+
+                View layoutView = LayoutInflater.from(viewGroup.getContext()).inflate(
+                        R.layout.item_content, viewGroup, false);
+
+                return new ItemViewHolder((layoutView));
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+        int viewType = getItemViewType(i);
+
+        switch (viewType) {
+            case TYPE:
+            default:
+
+                ItemViewHolder itemViewHolder = (ItemViewHolder) viewHolder;
+                Medicines medicineName = (Medicines) listRecyclerItem.get(i);
+
+                itemViewHolder.name.setText(medicineName.getName());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listRecyclerItem.size();
+    }
+}
