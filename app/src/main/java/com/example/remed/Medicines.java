412959f3f911b8e18bfc82314ba1491220bd2ef5
Fatmanur Özdemir <ozdemir.fatmanur18@gmail.com>
2020-05-27 22:55:21 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/remed/Medicines.java
@@ -0,0 +1,15 @@
+package com.example.remed;
+
+public class Medicines {
+
+    private final String name;
+
+    public Medicines(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
