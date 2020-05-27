95cae8f99b7af9a1faa99ff28948dd3c2684a6c7
Fatmanur Özdemir <ozdemir.fatmanur18@gmail.com>
2020-05-27 22:54:11 +0300

    Hata veriyor (line 27)

--- app/src/main/java/com/example/remed/SeeMedList.java
+++ app/src/main/java/com/example/remed/SeeMedList.java
@@ -1,64 +1,82 @@
 package com.example.remed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SeeMedList extends AppCompatActivity {
-    private String filename = AddMedicine.filename;
-    private TextView showMed;
 
+    private RecyclerView mRecyclerView;
+    private List<Object> viewItems = new ArrayList<>();
+
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    //private static final String TAG = "MainActivity";
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.see_med_list);
-        showMed = findViewById(R.id.textView12);
-    }
 
-    public void seeList(View v) {
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new RecyclerAdapter(this, viewItems);
+        mRecyclerView.setAdapter(mAdapter);
 
         try {
-            FileInputStream fin = openFileInput(filename);
-            int c;
-            StringBuilder temp= new StringBuilder();
-            while ((c = fin.read()) != -1) {
-                temp.append((char) c);
-            }
-            fin.close();
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast.makeText(context, temp, duration).show();
-            System.out.println(temp);
-
-            showMed.setText("");
-            showMed.append(temp);
-
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    private void add(){
-        try {
-            FileOutputStream fOut;
-            fOut = openFileOutput(filename,Context.MODE_PRIVATE);
-            String str = "test data";
-            fOut.write(str.getBytes());
-            fOut.close();
+            readDataFromFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+
+    private void readDataFromFile() throws IOException{
+
+        FileInputStream inputStream = null;
+        StringBuilder builder = new StringBuilder();
+
+        try
+        {
+            FileInputStream fin=new FileInputStream("/data/data/com.example.remed/files/listfile.txt");
+            char ch = (char) fin.read();
+            String mediName = null;
+
+            while ( ( ch = (char) fin.read()) != -1) {
+
+                while( ( ch = (char) fin.read()) != ' ' ){
+
+                    mediName += ch;
+
+                }
+                Medicines medicines = new Medicines(mediName);
+                viewItems.add(medicines);
+                mediName = null;
+            }
+
+            fin.close();
+
+        } catch(Exception e){
+            System.out.println(e);
+        }
+
     }
 }
